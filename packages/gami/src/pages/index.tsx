87025853662d55bf3c777efab923bc00a27618b3
Khoa-Bit <55542012+Khoa-bit@users.noreply.github.com>
2023-01-27 20:19:23 +0700

    feat: install Tiptap @ https://tiptap.dev/

--- packages/gami/src/pages/index.tsx
+++ packages/gami/src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { type NextPage } from "next";
+import Tiptap from "src/components/wysiwyg/TipTap";
 
 const Home: NextPage = () => {
   return (
     <>
       <h1>Redirecting...</h1>
+      <Tiptap />
     </>
   );
 };
